0172040f87fb57d2c08609e700844f1f1994d1ac
Polina <polina.prokofieva@gmail.com>
2020-05-28 19:37:03 +0300

    Updates

---
+++ precache-manifest.6b7f4f3e5a3a28764c51a4316163cc40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b625e4465c2a3af85ba383b930c6559",
+    "url": "/NASAinfo/index.html"
+  },
+  {
+    "revision": "134598cad2054ddf8783",
+    "url": "/NASAinfo/static/css/main.b714b72d.chunk.css"
+  },
+  {
+    "revision": "d0a34018d8fc0e600662",
+    "url": "/NASAinfo/static/js/2.862dd21b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/NASAinfo/static/js/2.862dd21b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134598cad2054ddf8783",
+    "url": "/NASAinfo/static/js/main.c9ec8400.chunk.js"
+  },
+  {
+    "revision": "e0aaea6dcc41d3225c6f",
+    "url": "/NASAinfo/static/js/runtime-main.993a3dcb.js"
+  },
+  {
+    "revision": "808231359c6f01afb772a34c2732bfb0",
+    "url": "/NASAinfo/static/media/JupiterClouds_JunoGill_960.80823135.jpg"
+  }
+]);
