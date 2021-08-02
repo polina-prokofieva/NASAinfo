a7fb30e6b366a3bea870cd5f1382b9dd60ce7e52
Polina Prokofieva <polina.prokofieva@gmail.com>
2021-08-02 19:09:43 +0300

    Switch day to yesterday and tomorrow

--- src/components/PictureOfTheDay/PictureOfTheDay.jsx
+++ src/components/PictureOfTheDay/PictureOfTheDay.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { isVideoContent, today } from '../../utils/common';
 import withData from '../hoc-helpers/withData';
+import { isVideoContent, isHTMLContent, videoAttrs } from '../../utils/common';
+import {
+    today, isToday, convertDateToString,
+    getNextDay, getPrevDay,
+    formatDate } from '../../utils/dateUtils';
 import styles from './PictureOfTheDay.module.scss';
 
 
@@ -16,41 +21,42 @@ const PictureOfTheDay = ({ data }) => {
   );
 };
 
-const PictureView = ({ url, hdurl, title, explanation, date = today }) => {
+const PictureOfTheDayView = ({
+  date, url, hdurl, title, explanation,
+  isVideo, isHTML,
+  toPrevDay, toNextDay
+}) => {
+  const graphic = isVideo ?
+    <iframe { ...videoAttrs } src={`${url}&controls=0`} /> :
+    <img alt={title} src={url} />;
+
+  console.log(isHTML);
+
+  const linkUrl = isHTML ? url : hdurl;
+
   return (
     <React.Fragment>
-      <div className={styles.image}>
-        <img
-          alt={title}
-          src={url}
-        />
-      </div>      
+      { !isHTML && <div className={styles.image}>
+        { graphic }
+      </div> }
       <div className={styles.description}>
-        <h2>{title}</h2>
-        <p>{explanation}</p>
-        <p className="download"><a href={hdurl} target="_blank" rel="noopener noreferrer">Download HD</a></p>
-      </div>
-    </React.Fragment>
-  );
-};
-
-const VideoView = ({ url, title, explanation }) => {
-  const modifiedUrl = `${url}&controls=0`;
-  return (
-    <React.Fragment>
-      <div className={styles.image}>
-        <iframe src={modifiedUrl}
-                title="Picture Of The Day"
-                frameBorder="0"
-                allow="accelerometer; autoplay; encrypted-media; picture-in-picture"
-                allowFullScreen
-        >
-        </iframe>
-
-      </div>
-      <div className={styles.description}>
-        <h2>{title}</h2>
-        <p>{explanation}</p>
+        <div className={styles.currentDate}>
+          <span className={ styles.yesterday } onClick={ toPrevDay }>◀</span>
+          <span className={ styles.today }>{ convertDateToString(date) }</span>
+          { !isToday(date) &&
+            <span className={ styles.tomorrow } onClick={ toNextDay }>▶</span>
+          }
+        </div>
+        <article className={styles.headerAndText}>
+          <h2>{title}</h2>
+          <p>{explanation}</p>
+        </article>
+        <p className="download">
+          { !isVideo && !isHTML &&
+            <a href={linkUrl} target="_blank" rel="noopener noreferrer">Download HD</a>
+          }&nbsp;
+          { isHTML && <a href={url} target="_blank">Go to picture of the day</a>}
+        </p>
       </div>
     </React.Fragment>
   );
