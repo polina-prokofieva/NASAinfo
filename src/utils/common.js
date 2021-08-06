d2ce84f685c10c52da02b3dddef3b0ce51ca736e
Polina Prokofieva <polina.prokofieva@gmail.com>
2021-08-06 21:52:37 +0300

    Animation on render data

--- src/utils/common.js
+++ src/utils/common.js
@@ -4,4 +4,12 @@ const isVideoContent = (url) => youTubeReg.test(url);
 
 const today = new Date();
 
-export { isVideoContent, today };
+const getClasses = classList => classList.reduce((acc, className) => {
+  return `${acc} ${className}`;
+}, '');
+
+export {
+  isVideoContent,
+  today,
+  getClasses
+};
