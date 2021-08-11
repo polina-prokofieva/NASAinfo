71b4c41923248ef6c441ee058f8edaa520c27f07
Polina Prokofieva <polina.prokofieva@gmail.com>
2021-08-11 21:32:36 +0300

    Menu animation on load

--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { topRoutes } from '../../routes';
 import styles from './TopMenu.module.scss';
@@ -7,15 +7,25 @@ const TopMenu = () => {
   return (
     <nav className={styles.TopMenu}>
       <ul>
-        { topRoutes.map(({name, path}) => (
-          <li key={name}>
-            <NavLink to={path} activeClassName={styles.selected}>
-              <span>{name}</span>
-            </NavLink>
-          </li>
-        )) }
+        { routes.map((route, i) => <MenuItem {...route} order={i} />) }
       </ul>
     </nav>
+  );
+};
+
+const MenuItem = ({ name, path, order }) => {
+  const [isRendered, setIsRendered] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsRendered(true), 200 * order);
+  }, []);
+
+  return (
+    <li key={name} className={ isRendered ? styles.rendered : '' }>
+      <NavLink to={path} activeClassName={styles.selected}>
+        <span>{name}</span>
+      </NavLink>
+    </li>
   );
 };
 
